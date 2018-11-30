064dedef0c967031389e66638439cbc37336c9ca
Julian Israel <jdelacruz@crestviewc.com>
2018-11-30 13:15:17 -0400

    separating process

--- src/parseus.ts
+++ src/parseus.ts
@@ -2,26 +2,36 @@ import parsers from './parsers'
 import { getMetaKey } from './helpers/utils'
 import processor from './helpers/processors'
 
-export default function parseus<T>(ctx: T) {
+interface IOptions<T> {
+  model: any
+  ctx: T
+}
+
+function process<T>(options: IOptions<T>): T {
+  const { model, ctx } = options
   const isArray = Array.isArray(ctx)
+  const meta = new model()
+  const keys = Object.keys(meta)
+
+  if (isArray) {
+    return (ctx as any).map(item => parseus(item).to(model))
+  }
+
+  return keys.reduce(
+    (acc, key) => {
+      const metaKey = getMetaKey(key)
+      const entries = { ctx, metaKey, meta }
+      const options = { acc, key, parsers, entries }
+      return processor(options)
+    },
+    {} as T
+  )
+}
+
+export default function parseus<T>(ctx: T) {
   return {
     to(model): T {
-      const meta = new model()
-      const keys = Object.keys(meta)
-
-      if (isArray) {
-        return (ctx as any).map(item => parseus(item).to(model))
-      }
-
-      return keys.reduce(
-        (acc, key) => {
-          const metaKey = getMetaKey(key)
-          const entries = { ctx, metaKey, meta }
-          const options = { acc, key, parsers, entries }
-          return processor(options)
-        },
-        {} as T
-      )
+      return process({ model, ctx })
     }
   }
 }
